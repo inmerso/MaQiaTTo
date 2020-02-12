7090a7b9de4911afa630a9e589d091174f28ec78
inmerso <luca.rizzon@gmail.com>
2020-02-12 10:28:05 +0100

    use json messages

--- publisher.py
+++ publisher.py
@@ -1,15 +1,23 @@
 from paho.mqtt.client import Client
+from datetime import datetime
+import json
 
 if __name__ == "__main__":
 
     client = Client("localhost:1883")
     client.connect("localhost")
 
+    message = {}
+    message['Sensor'] = "MySensor"
+
     while True:
         a = input()
+        message['Time'] = (datetime.now()).strftime("%d-%b-%Y %H:%M:%S:%f")
+        message['Value'] = a
+        message_json = json.dumps( message )
         if a != "0":
-            client.publish("DummyTopic", a)
+            client.publish("DummyTopic", message_json)
         else:
             break
-
+    
     client.disconnect()
