04ee89ace3372f37dd66a09f7627474bbea2d8b4
inmerso <luca.rizzon@gmail.com>
2020-02-18 14:09:21 +0100

    add subscriber disconnect on 0

--- subscriber.py
+++ subscriber.py
@@ -1,22 +1,37 @@
 from paho.mqtt.client import Client
 from databaseHandler import db_add
 
-def my_on_connect(client, userdata, flags, rc):
+def connect_callback(client, userdata, flags, rc):
     print("Successfully connected")
 
-def my_on_message(client, userdata, message):
-    print("Received Message: " + str(message.payload))
+def message_callback(client, userdata, message):
+    # print("Received Message: " + str(message.payload))
     db_add(message.topic, message.payload)
 
+def disconnect_callback(client, userdata, rc):
+    print("Disconnected")
 
 if __name__ == "__main__":
     
     client = Client("localhost")
 
-    client.on_connect = my_on_connect
-    client.on_message = my_on_message
+    client.on_connect = connect_callback
+    client.on_message = message_callback
+    client.on_disconnect = disconnect_callback
 
     client.connect("localhost")
     client.subscribe("DummyTopic")
 
-    client.loop_forever()
+    client.loop_start()
+
+    while True:
+        a = input()
+        try:
+            a = int(a)
+        except ValueError as identifier:
+            print("type 0 to stop")
+            continue
+        if a == 0:
+            client.loop_stop()
+            client.disconnect()
+            exit()
